bd7191a10b8e124a102bc42ecbfc5fc2aee7e932
Piotr Palucki <piotr.palucki@motorolasolutions.com>
2017-06-19 12:42:57 +0200

    Added simple strategy tests to demonstrate usage

--- StrategyTest.cpp
+++ StrategyTest.cpp
@@ -1,25 +1,92 @@
-#include <iostream>
 #include "doctest.h"
 
-int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+#include <iostream>
+#include <memory>
 
+// based on http://www.bogotobogo.com/DesignPatterns/strategy.php
+
+//Strategy
+class SortBehavior
+{
+public:
+    virtual ~SortBehavior() { }
+    virtual void sort() const = 0;
+};
+
+class ByDate: public SortBehavior
+{
+public:
+    virtual void sort() const override
+    {
+        std::cout << "ByDate sort()\n";
+    }
+};
+
+class ByAuthor: public SortBehavior {
+public:
+    virtual void sort() const override
+    {
+        std::cout << "ByAuthor sort()\n";
+    }
+};
+
+class ByGenre: public SortBehavior
+{
+public:
+    virtual void sort() const override
+    {
+        std::cout << "ByGenre sort()\n";
+    }
+};
+
+// Context
+class Collection
+{
+private:
+    SortBehavior* m_sort;
+    std::string m_collectionName;
+public:
+    Collection(const std::string& name) : m_sort(nullptr), m_collectionName(name) {}
+    void set_sort(SortBehavior* s)
+    {
+        m_sort = s;
+    }
+    void sort() const
+    {
+        std::cout << "Sorting " << m_collectionName << " with ";
+        m_sort->sort();
+    }
+};
 
 TEST_SUITE_BEGIN("StrategyTest.cpp");
 
-TEST_CASE("testing the factorial function")
+TEST_CASE("Encapsulate a family of algorithm into their own set of classes")
 {
-    CHECK(factorial(1) == 1);
-    CHECK(factorial(2) == 2);
-    CHECK(factorial(3) == 6);
-    CHECK(factorial(10) == 3628800);
+    ByDate      sortByDate;
+    ByAuthor    sortByAuthor;
+
+    Collection  books("books");
+    Collection  newspapers("newspapers");
+
+    books.set_sort(&sortByAuthor);
+    books.sort();
+
+    newspapers.set_sort(&sortByDate);
+    newspapers.sort();
 }
 
-TEST_CASE("testing the factorial function2")
+TEST_CASE("Change behavior at run time")
 {
-    CHECK(factorial(1) == 1);
-    CHECK(factorial(2) == 2);
-    CHECK(factorial(3) == 6);
-    CHECK(factorial(10) == 3628800);
+    ByAuthor    sortByAuthor;
+    ByGenre     sortByGenre;
+
+    Collection  books("books");
+
+    books.set_sort(&sortByAuthor);
+    books.sort();
+
+    books.set_sort(&sortByGenre);
+    books.sort();
 }
 
 TEST_SUITE_END();
