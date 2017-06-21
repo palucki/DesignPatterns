c6bee0241b3f90e80b723de597d1bf182f92977e
Piotr Palucki <piotr.palucki@motorolasolutions.com>
2017-06-21 15:36:57 +0200

    Added template for observer:

---
+++ Observer.cpp
@@ -0,0 +1,49 @@
+#include "doctest.h"
+
+#include <iostream>
+
+
+//Zrobic tutaj prosty przyklad
+
+// 3 miejsca w ktorych jest potrzebna notyfikacja
+// i dodac recznie 4 miejsce albo inny paramter
+
+// potem 3 miejsca z observerem i dodanie 4 miejsca - jakie jest proste
+
+//pozniej sie przelaczyc na rzecyzwisty przyklad
+
+// pokazac ze mozna zmienic w czasie wykonywania
+// pokzazc ze mozna robic rozne histerezy (setChanged)                    PUSH vs PULL
+
+
+//cos co uzytkownik bedzie mogl rozwijac sam
+
+// GIELDA JAKO PRZYKLAD !!!!!!!!!!!
+// notyfikacja na ekranach
+// notyfikacja na lampce / glosniku przy kazdej zmianie
+// notyfikacja na telefonach uzytkownikow
+// notyfikacja mailem do zainteresownaych
+
+class Display
+{
+    virtual void updateData(double value) = 0;
+};
+
+class Stock
+{
+private:
+    double value;
+};
+
+TEST_SUITE_BEGIN("Observer.cpp");
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Now the Sun of the Design Patterns shine...
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+TEST_SUITE_END();
